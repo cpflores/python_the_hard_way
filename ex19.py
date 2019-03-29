1baeaaf5897c12f689e23fc1b3423691a3b3d760
Chadwick Flores <chadwickflores@gmail.com>
2019-03-29 14:53:33 -0500

    complete exercise 19

--- ex19.py
+++ ex19.py
@@ -4,4 +4,22 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-cheese_and_crackers(1, 5)
+# calls function, passing two arguments
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+# gives values to variables
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+# calls function, passing two variables for arguements
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+# calls function, passes value math for arguements 
+print("We can even do math innside too:")
+cheese_and_crackers(10 + 20, 5 + 6)
+
+# calls function, passes variables for values and math for arguements
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
